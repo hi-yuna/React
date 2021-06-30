d85ba97517392b43c9490f594c80187e78f952db
yuna <ouna0223@naver.com>
2021-07-01 00:58:48 +0900

    Prettier

---
+++ begin-react25/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+const a = 'yuna1';
+const b = 'yuna2';
+
+function App() {
+  return (
+    <div>
+      <div>yuna</div>
+      <ul>
+        <li>유나유나</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
